b7d04bf20e476b8e52dafd1849d98d77bd90e184
Jacob Eckert <jweckert17@gmail.com>
2020-11-23 10:44:32 -0600

    query top 2009 sales agents via max and subquery

--- queries/top_2009_agent.sql
+++ queries/top_2009_agent.sql
@@ -12,3 +12,19 @@ AND i.InvoiceDate >= '2009-01-01' AND i.InvoiceDate <= '2009-12-31'
 GROUP BY e.EmployeeId
 ORDER BY TotalSales DESC
 LIMIT 1;
+
+/*
+Provide a query that shows total sales made by each sales agent (using max + subquery).
+*/
+SELECT EmployeeId, EmployeeFullName, MAX(TotalSales)
+FROM (
+  SELECT e.EmployeeId, e.FirstName || ' ' || e.LastName as EmployeeFullName, SUM(i.Total) as TotalSales
+  FROM Employee e
+  JOIN Customer c
+  ON c.SupportRepId = e.EmployeeId
+  JOIN Invoice i
+  ON i.CustomerId = c.CustomerId
+  WHERE Title = 'Sales Support Agent'
+  AND i.InvoiceDate >= '2009-01-01' AND i.InvoiceDate <= '2009-12-31'
+  GROUP BY e.EmployeeId
+);
