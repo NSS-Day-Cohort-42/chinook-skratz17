2bcca503652ba0c7b3766f1988d3ee44d91a7883
Jacob Eckert <jweckert17@gmail.com>
2020-11-23 10:56:18 -0600

    querying for most purchased track of 2013

---
+++ queries/top_2013_track.sql
@@ -0,0 +1,14 @@
+/*
+Provide a query that shows the most purchased track of 2013.
+*/
+SELECT Name, MAX(PurchaseCount)
+FROM (
+  SELECT t.Name, COUNT(*) as PurchaseCount
+  FROM Track t
+  JOIN InvoiceLine il
+  ON il.TrackId = t.TrackId
+  JOIN Invoice i
+  ON il.InvoiceId = i.InvoiceId
+  WHERE i.InvoiceDate >= '2013-01-01' AND i.invoiceDate <= '2013-12-31'
+  GROUP BY t.TrackId
+);
