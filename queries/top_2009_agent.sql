0dc1e1f3f2888d8c575b57ad9fb07152456842de
Jacob Eckert <jweckert17@gmail.com>
2020-11-23 10:28:13 -0600

    query for the top-selling sales agent in 2009

---
+++ queries/top_2009_agent.sql
@@ -0,0 +1,14 @@
+/*
+Provide a query that shows total sales made by each sales agent.
+*/
+SELECT e.EmployeeId, e.FirstName || ' ' || e.LastName as EmployeeFullName, SUM(i.Total) as TotalSales
+FROM Employee e
+JOIN Customer c
+ON c.SupportRepId = e.EmployeeId
+JOIN Invoice i
+ON i.CustomerId = c.CustomerId
+WHERE Title = 'Sales Support Agent'
+AND i.InvoiceDate >= '2009-01-01' AND i.InvoiceDate <= '2009-12-31'
+GROUP BY e.EmployeeId
+ORDER BY TotalSales DESC
+LIMIT 1;
