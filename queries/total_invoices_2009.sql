86b2e593769e7b657745c67eb7d6486975860643
Jacob Eckert <jweckert17@gmail.com>
2020-11-23 09:06:33 -0600

    query for 2009 and 2011 invoices

---
+++ queries/total_invoices_2009.sql
@@ -0,0 +1,2 @@
+SELECT * FROM Invoice
+WHERE InvoiceDate >= '2009-01-01' AND InvoiceDate <= '2009-12-31';
---
+++ queries/total_invoices_2011.sql
@@ -0,0 +1,2 @@
+SELECT * FROM Invoice
+WHERE InvoiceDate >= '2011-01-01' AND InvoiceDate <= '2011-12-31';
